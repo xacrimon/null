ef75377be80b6ab784aaa0d330a44b5717b0f849
Joel Wejdenstål <jwejdenstal@icloud.com>
2022-05-02 23:36:02 +0200

    hello world

--- null-web/src/App.tsx
+++ null-web/src/App.tsx
@@ -1,21 +1,13 @@
 import React from "react";
 
 function App() {
+  const style = {
+    backgroundColor: "#ffffff",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={style} className="App">
+      <h1>Hello World!</h1>
     </div>
   );
 }
