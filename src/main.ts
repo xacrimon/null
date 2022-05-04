3d4762f50b05af388084394ece77d0b11bb493ac
Joel Wejdenstål <jwejdenstal@icloud.com>
2022-05-04 16:05:44 +0200

    set pragmas

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import fastify from "fastify";
+import openSqlite3 from "better-sqlite3"
 
 const server = fastify();
+const db = openSqlite3("./null.dat");
+
+db.pragma("analysis_limit = 1000")
+db.pragma("automatic_index = false")
+db.pragma("busy_timeout = 10")
+db.pragma("cache_size = -8192")
+db.pragma("journal_mode = wal")
+db.pragma("synchronous = normal")
 
 server.get("/", async () => {
   return "Hello World!";
@@ -11,5 +20,6 @@ server.listen(8080, (err, address) => {
     console.error(err);
     process.exit(1);
   }
+
   console.log(`Server listening at ${address}`);
 });
