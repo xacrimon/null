b1ea7ec052c079e341b837454c9a700408fe771b
Joel Wejdenstål <jwejdenstal@icloud.com>
2022-05-19 00:06:40 +0200

    Add a super-basic editor

--- null-web/src/App.tsx
+++ null-web/src/App.tsx
@@ -1,4 +1,5 @@
 import NavBar from "./components/NavBar";
+import Editor from "./components/Editor";
 
 function App() {
   const style = {
@@ -8,6 +9,7 @@ function App() {
   return (
     <div style={style} className="App">
       <NavBar />
+      <Editor />
     </div>
   );
 }
