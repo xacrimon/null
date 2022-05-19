519249bca79888551b3a743739794de277ff2819
Joel Wejdenstål <jwejdenstal@icloud.com>
2022-05-19 23:40:02 +0200

    sprinkle some indexes

--- migrations/0001-init.sql
+++ migrations/0001-init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE pastes (
-    id INTEGER PRIMARY KEY,
-    author_account INTEGER NOT NULL,
+    id INTEGER NOT NULL PRIMARY KEY,
+    author_id INTEGER NOT NULL,
     created INTEGER NOT NULL,
     expiry INTEGER,
     title TEXT,
@@ -8,28 +8,36 @@ CREATE TABLE pastes (
     content TEXT NOT NULL
 );
 
+CREATE INDEX pastes_author_id_index ON pastes (author_id);
+
 CREATE TABLE saved_pastes (
-    paste_id INTEGER NOT NULL,
-    account_id INTEGER NOT NULL
+    account_id INTEGER NOT NULL,
+    paste_id INTEGER NOT NULL
 );
 
+CREATE INDEX saved_pastes_account_id_index ON saved_pastes (account_id);
+
 CREATE TABLE accounts (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL,
-    contact_email TEXT NOT NULL,
+    email TEXT NOT NULL,
     public_email TEXT,
     display_name TEXT,
     pronouns TEXT
 );
 
+CREATE UNIQUE INDEX accounts_username_index ON accounts (username);
+CREATE UNIQUE INDEX accounts_email_index ON accounts (email);
+
 CREATE TABLE identities (
     account_id INTEGER NOT NULL,
-    provider TEXT,
-    pw_hash TEXT
+    provider TEXT NOT NULL,
+    pw_hash TEXT,
+    PRIMARY KEY (account_id, provider)
 );
 
 CREATE TABLE refresh_tokens (
-    token TEXT PRIMARY KEY,
+    token TEXT NOT NULL PRIMARY KEY,
     account_id INTEGER NOT NULL,
     expiry INTEGER NOT NULL
 );
