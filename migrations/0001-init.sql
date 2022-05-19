0158a91174ef9d7b0b8b8421c6c2ac5af8ecaac8
Joel Wejdenstål <jwejdenstal@icloud.com>
2022-05-19 11:25:23 +0200

    add tables accounts, identities and refresh_tokens

--- migrations/0001-init.sql
+++ migrations/0001-init.sql
@@ -7,3 +7,24 @@ CREATE TABLE pastes (
     lang TEXT NOT NULL,
     content TEXT NOT NULL
 );
+
+CREATE TABLE accounts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    contact_email TEXT NOT NULL,
+    public_email TEXT,
+    display_name TEXT,
+    pronouns TEXT
+);
+
+CREATE TABLE identities (
+    account_id INTEGER NOT NULL,
+    provider TEXT,
+    pw_hash TEXT
+);
+
+CREATE TABLE refresh_tokens (
+    token TEXT PRIMARY KEY,
+    account_id INTEGER NOT NULL,
+    expiry INTEGER NOT NULL
+);
